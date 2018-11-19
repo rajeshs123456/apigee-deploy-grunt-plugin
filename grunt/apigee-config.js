82f1cddcc32133fe0b39ca37a421fead91cff787
rajeshs123456 <45097005+rajeshs123456@users.noreply.github.com>
2018-11-19 11:36:25 +0530

    Update apigee-config.js

--- grunt/apigee-config.js
+++ grunt/apigee-config.js
@@ -3,11 +3,11 @@ exports.profiles = function(grunt){
 			env : grunt.option('env'),     // replace with environment
 			'test' : {
 					apiproxy : 'forecastweather-grunt-plugin-api',
-					org : 'shivamadishet-trial', // replace with organization
+					org : 'shivam069-eval', // replace with organization
 					env : 'test',     // replace with environment
 					url_mgmt : 'https://api.enterprise.apigee.com',  // for cloud environments, leave as is
-					username : grunt.option('username'), //|| process.env.ae_username, // pass credentials as arguments as grunt task --username=$ae_username --password=$ae_password
-					password : grunt.option('password'), //|| process.env.ae_password, // use ae_username and ae_password are defined as environment variables and no arguments are passed
+					username : 'shivam069@yahoo.com' //|| process.env.ae_username, // pass credentials as arguments as grunt task --username=$ae_username --password=$ae_password
+					password : '@Test1234', //|| process.env.ae_password, // use ae_username and ae_password are defined as environment variables and no arguments are passed
 					revision : grunt.option('revision'), // provide revision to be undeployed by passing argument as --revision=X
 			        override : grunt.option('override') || true,
 			        delay : grunt.option('delay') || 10
